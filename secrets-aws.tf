bd7d6371571363395d90090705bb8e62883bb74e
guyzsarun <sarun.nunt@gmail.com>
2024-01-21 10:04:45 +0100

    add aws secrets

---
+++ secrets-aws.tf
@@ -0,0 +1,9 @@
+resource "vault_aws_secret_backend_role" "aws_role" {
+  backend = vault_aws_secret_backend.aws.path
+  name    = split(".",each.value)[0]
+  credential_type = "iam_user"
+
+  policy_document = file("${path.module}/secrets/aws/${each.value}")
+
+  for_each = fileset("${path.module}/secrets/aws", "*")
+}
