7f470e7c914e791a8c08e70cc24b302312050f6b
guyzsarun <sarun.nunt@gmail.com>
2024-01-25 21:43:53 +0100

    fmt

--- variables.tf
+++ variables.tf
@@ -1,14 +1,24 @@
 variable "aws_access_key" {
-  type = string
+  type      = string
   sensitive = true
 }
 
 variable "aws_secret_key" {
-  type = string
+  type      = string
   sensitive = true
 }
 
 variable "aws_region" {
-  type = string
+  type      = string
   sensitive = true
+}
+
+variable "postgres" {
+  sensitive = true
+  type = object({
+    host     = string
+    port     = number
+    username = string
+    password = string
+  })
 }
