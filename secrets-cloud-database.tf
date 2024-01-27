eddb37eb3c4b2523f4517d4fa2ce9aaf854d184d
guyzsarun <sarun.nunt@gmail.com>
2024-01-27 08:37:21 +0100

    add cloud db

---
+++ secrets-cloud-database.tf
@@ -0,0 +1,31 @@
+resource "vault_mount" "cloud_database" {
+  path = "cloud"
+  type = "database"
+
+  description = "Cloud database secret engine"
+}
+
+resource "vault_database_secret_backend_connection" "mongo" {
+  backend = vault_mount.cloud_database.path
+  name    = "mongo"
+  allowed_roles = [
+    "mongo_readonly"
+  ]
+
+  mongodb {
+    connection_url = "mongodb://{{username}}:{{password}}@${var.mongo.host}:${var.mongo.port}"
+    username       = var.mongo.username
+    password       = var.mongo.password
+  }
+}
+
+resource "vault_database_secret_backend_role" "mongo_role" {
+  backend             = vault_mount.cloud_database.path
+  name                = split(".",each.value)[0]
+  db_name             = vault_database_secret_backend_connection.mongo.name
+  creation_statements = [
+    file("${path.module}/secrets/database/mongo/${each.value}")
+  ]
+  default_ttl         = 3600
+  for_each = fileset("${path.module}/secrets/database/mongo", "*.json")
+}
