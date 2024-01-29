0ad5aa2e7c1b6292c6acaf1cfad925c068236965
guyzsarun <sarun.nunt@gmail.com>
2024-01-29 20:12:59 +0100

    fmt

--- secrets-database.tf
+++ secrets-database.tf
@@ -6,9 +6,9 @@ resource "vault_mount" "homelab" {
 }
 
 resource "vault_database_secret_backend_connection" "postgres" {
-  backend = vault_mount.homelab.path
-  name    = "postgres"
-  allowed_roles = [ for role in fileset("${path.module}/secrets/database/postgres", "*.sql") : split(".",role)[0]]
+  backend       = vault_mount.homelab.path
+  name          = "postgres"
+  allowed_roles = [for role in fileset("${path.module}/secrets/database/postgres", "*.sql") : split(".", role)[0]]
 
   postgresql {
     connection_url = "host=${var.postgres.host} port=${var.postgres.port} user={{username}} password={{password}} sslmode=disable"
@@ -19,10 +19,10 @@ resource "vault_database_secret_backend_connection" "postgres" {
 
 resource "vault_database_secret_backend_role" "postgres_role" {
   backend             = vault_mount.homelab.path
-  name                = split(".",each.value)[0]
+  name                = split(".", each.value)[0]
   db_name             = vault_database_secret_backend_connection.postgres.name
   creation_statements = [file("${path.module}/secrets/database/postgres/${each.value}")]
 
-  default_ttl         = 3600
-  for_each = fileset("${path.module}/secrets/database/postgres", "*.sql")
+  default_ttl = 3600
+  for_each    = fileset("${path.module}/secrets/database/postgres", "*.sql")
 }
