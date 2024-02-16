61fa4b1d75e18418df361551a4d4d2a708612bbf
guyzsarun <sarun.nunt@gmail.com>
2024-02-16 21:10:35 +0100

    add main

---
+++ main.tf
@@ -0,0 +1,16 @@
+module "policy" {
+  source = "./policy"
+}
+
+module "authentication" {
+  source = "./authentication"
+  path   = "userpass"
+}
+
+module "secrets-aws" {
+  source = "./secrets/aws"
+
+  aws_access_key = var.aws_access_key
+  aws_secret_key = var.aws_secret_key
+  aws_region = var.aws_region
+}
